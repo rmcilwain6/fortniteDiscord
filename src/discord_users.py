6e761fd3fc4c0ae4cb5ebfa8517951167e641e4b
Oliver Lewis <oliverlewis@uvic.ca>
2018-06-08 04:40:06 -0700

    added a \!run command. Only gets users right now.

--- src/discord_users.py
+++ src/discord_users.py
@@ -8,6 +8,8 @@ TOKEN = 'NDQ4NTQ5MDgyNzc2OTkzODAz.DeXv0A.02Hqp8M2dUleupLlOeEa-SpdOqw'
 
 client = discord.Client()
 
+botName = 'scraper_bot'
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -21,10 +23,27 @@ async def on_message(message):
         #get a list of all members in the server not including itself
         mem_list = message.server.members
         for member in mem_list:
-            if member.name != 'scraper_bot':
+            if member.name != botName:
                 print(member.name)
                 msg = member.name
                 await client.send_message(message.channel, msg)
+    
+    elif message.content.startswith('!run'):
+        print('running full process...')
+
+        mem_list = message.server.members
+
+        # members is a list of all the user names
+        members = [member.name for member in mem_list if member.name != botName]
+
+        await client.send_message(message.channel, 'getting members...')
+        for member in members:
+            await client.send_message(message.channel, member)
+        
+        
+        
+                
+
 
 @client.event
 async def on_ready():
